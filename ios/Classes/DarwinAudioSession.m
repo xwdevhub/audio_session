5433f42382c40a461c73b66e88f1d5047205ba9b
devlin <bhlin790@aliyun.com>
2022-04-18 14:09:52 +0800

    清除未知类型

--- ios/Classes/DarwinAudioSession.m
+++ ios/Classes/DarwinAudioSession.m
@@ -277,7 +277,10 @@ static NSHashTable<DarwinAudioSession *> *sessions = nil;
 - (NSMutableArray *)encodePortList:(NSArray<AVAudioSessionPortDescription *> *)ports {
     NSMutableArray *array = [NSMutableArray new];
     for (int i = 0; i < ports.count; i++) {
-        [array addObject:[self encodePort:ports[i]]];
+        NSDictionary *dict = [self encodePort:ports[i]];
+        if (dict != nil) {
+            [array addObject:dict];
+        }
     }
     return array;
 }
@@ -286,6 +289,9 @@ static NSHashTable<DarwinAudioSession *> *sessions = nil;
     BOOL hasHardwareVoiceCallProcessing = NO;
     if (@available(iOS 10.0, *)) {
         hasHardwareVoiceCallProcessing = port.hasHardwareVoiceCallProcessing;
+    }
+    if ([self encodePortType:port.portType] == nil) {
+        return nil;
     }
     return @{
         @"portName": port.portName,
